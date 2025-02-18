5d93b132cdcc72b34dd99b16e80c82232f9af8dc
johnny-emp <johnny@empyrealsdk.com>
2025-02-17 21:16:00 -0600

    rework asyncio testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,11 @@
+import asyncio
 import logging
 import os
 
 import pytest
 from eth_rpc import set_alchemy_key
+
+pytest_plugins = ("pytest_asyncio",)
 
 
 @pytest.fixture(autouse=True)
@@ -10,3 +13,13 @@ async def setup_tests():
     """Create a test runner instance with mock components."""
     set_alchemy_key(os.getenv("ALCHEMY_KEY"))
     logging.getLogger("dowse").setLevel(logging.DEBUG)
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+    yield loop
+    loop.close()
