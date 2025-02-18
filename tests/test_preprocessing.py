c134ccf91b9fe33a42764fa63e71560b00e02c3a
johnny-emp <johnny@empyrealsdk.com>
2025-02-18 00:17:05 -0600

    fixup

--- tests/test_preprocessing.py
+++ tests/test_preprocessing.py
@@ -23,7 +23,7 @@ async def test_pre_processor():
     assert formatted.error_message is None
 
 
-@pytest.mark.asyncio(loop_scope="session")
+@pytest.mark.asyncio()
 async def test_pre_processor_transfer():
     formatted = await ProcessTokens().format(
         Tweet(
