4adbaa1cfb47dbde654da8929036daf9b07b302c
Quincybob <74205675+Quincybob@users.noreply.github.com>
2025-02-25 03:53:25 +0800

    api paths

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,27 @@ const nextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "/:path*",
+        destination: "/api/:path*", // Keep the /api prefix
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,POST,PUT,DELETE,OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-OpenAI-Key",
+          },
+        ],
       },
     ];
   },
